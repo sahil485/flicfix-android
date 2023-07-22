570f431dd1f8322ea45aa282abcf6d5478569dfa
sahil485 <sahil.chatiwala@gmail.com>
2023-07-22 02:34:07 -0400

    added scheduled function to run everyday for cleanup of firebase storage

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,17 @@
 const {onCustomEventPublished} = require("firebase-functions/v2/eventarc");
 const logger = require("firebase-functions/logger");
 const {onCall} = require("firebase-functions/v2/https");
-const {initializeApp} = require("firebase-admin/app");
+const {initializeApp, cert} = require("firebase-admin/app");
 const admin = require('firebase-admin');
+const {getStorage} = require('firebase-admin/storage');
+const scheduler = require('firebase-functions/v2/scheduler')
 
-const app = initializeApp();
+var serviceAccount = require('./serviceAccount.json');
+
+const app = initializeApp({
+    credential: cert(serviceAccount),
+    storageBucket: 'flicfix.appspot.com'
+});
 
 exports.onimageresized = onCustomEventPublished(
     "firebase.extensions.storage-resize-images.v1.complete",
@@ -17,3 +24,20 @@ exports.onimageresized = onCustomEventPublished(
               };
             return admin.messaging().send(payload)
     });
+
+exports.strorageCleanup = scheduler.onSchedule("0 0 * * * ", async(event) =>{
+    const flicBucket = getStorage().bucket()
+    const folder = 'editable/'
+    const oneHourAgo = Date.now() - 60*60*1000
+    const results = flicBucket.getFiles({
+        prefix: folder
+    })
+    .then((res) => {
+        const filesToDelete = []
+        res[0].forEach((file) => {
+            if(Date.parse(file.metadata.timeCreated) < parseInt(oneHourAgo.toString())) filesToDelete.push(file)
+        })
+        return Promise.all(filesToDelete.map((file) => file.delete()))
+    })
+    return results
+});
--- src/bottom_bar/styles.js
+++ src/bottom_bar/styles.js
@@ -6,19 +6,10 @@ const styles = StyleSheet.create({
     position  : 'absolute', 
     bottom: 20, 
     left : 20, 
-    right: 20, 
-    elevation: 0, 
+    right: 20,
     backgroundColor: '#E5E4E2',
     borderRadius: 15,
     height: 50,
-    shadowColor: '#9e9e9e',
-    shadowOffset: {
-      width: 0,
-      height: 10
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
     justifyContent: 'space-evenly'
   },
 
@@ -33,14 +24,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     top: 0,
-    shadowColor: '#7F5DF0',
-    shadowOffset: {
-      width: 0,
-      height: 10
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5,
     marginHorizontal: 30,
   },
 });
